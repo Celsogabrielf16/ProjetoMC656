99bccfc3b5ab3b2dceb015af8a80518a2df30fd7
Celso Gabriel Prado <celsogabrielf@gmail.com>
2025-04-06 12:12:20 -0300

    Login service implementation

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -9,3 +9,12 @@ exports.login = async (req, res) => {
     res.status(401).json({ error: err.message });
   }
 };
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await userService.getAllUsers();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
--- backend/src/db/client.js
+++ backend/src/db/client.js
@@ -1,9 +1,9 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    connectionString: 'postgresql://user:ArnOzYra9ZTxQ4qFJJdH4AsTc5gH5lo2@dpg-cvotgj7gi27c73atl000-a.oregon-postgres.render.com/bikes_gmit', // Substitua por sua URL do Render
+    connectionString: 'postgresql://user:ArnOzYra9ZTxQ4qFJJdH4AsTc5gH5lo2@dpg-cvotgj7gi27c73atl000-a.oregon-postgres.render.com/bikes_gmit',
     ssl: {
-        rejectUnauthorized: false, // Necessário para conexões seguras no Render
+        rejectUnauthorized: false,
     },
 });
 
@@ -11,12 +11,4 @@ client.connect()
     .then(() => console.log('Connected to PostgreSQL'))
     .catch(err => console.error('Connection error', err.stack));
 
-client.query('SELECT * FROM public.users', (err, res) => {
-    if (err) {
-        console.error('Error executing query', err.stack);
-    } else {
-        console.log('Query result:', res.rows);
-        client.end(); // Fechar a conexão após a consulta
-    }
-})
-
+module.exports = client;
