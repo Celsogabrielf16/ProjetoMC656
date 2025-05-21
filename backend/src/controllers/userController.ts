0060f56c7d39b54f34bacad4be03c59e5e46f040
Vitor Lacerda <floco.vitor@gmail.com>
2025-05-20 22:49:16 -0300

    Refactor code to fix detected code smells

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import * as userService from '../services/userService';
+import { UserErrorHandler } from '../utils/userErrorHandler';
 
 export class UserController {
   public static async login(req: Request, res: Response) {
@@ -10,12 +11,9 @@ export class UserController {
       const token = await userService.login(email, password);
       res.status(200).json({ token });
     } catch (error) {
-      if (error instanceof Error && error.message === 'Credenciais inválidas')
-        return res.status(401).json({ error: error.message });
-
-      res.status(500).json({ error: 'Erro interno no servidor ao fazer login' });
+      return UserErrorHandler.handleLoginError(error, res);
     }
-  };
+  }
 
   public static async register(req: Request, res: Response) {
     try {
@@ -26,25 +24,16 @@ export class UserController {
       const user = await userService.register(name, email, password);
       res.status(201).json(user);
     } catch (error) {
-      if (error instanceof Error && error.message === 'Usuário já existe')
-        return res.status(409).json({ error: error.message });
-      
-      else if (error instanceof Error && error.message === 'Erro ao fazer o cadastro')
-        return res.status(400).json({ error: error.message });
-
-      res.status(500).json({ error: 'Erro interno no servidor ao fazer o cadastro' });
+      return UserErrorHandler.handleRegisterError(error, res);
     }
-  };
+  }
 
   public static async getAllUsers(req: Request, res: Response) {
-      try {
-          const users = await userService.getAllUsers();
-          return res.status(200).json(users);
-      } catch (error) {
-          if (error instanceof Error && error.message === 'Nenhum usuário encontrado') {
-              return res.status(404).json({ error: error.message });
-          }
-          res.status(500).json({ error: 'Erro interno no servidor ao buscar todos os usuários' });
-      }
-  };
+    try {
+      const users = await userService.getAllUsers();
+      return res.status(200).json(users);
+    } catch (error) {
+      return UserErrorHandler.handleGetAllUsersError(error, res);
+    }
+  }
 }
