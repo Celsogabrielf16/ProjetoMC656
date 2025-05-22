64740eda3ee46fb3b3849260f311e4f9a75d3985
Vitor Lacerda <floco.vitor@gmail.com>
2025-05-21 21:06:05 -0300

    Refactoring of Change Preventers code smell - Shotgun Surgery

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import * as userService from '../services/userService';
-import { UserErrorHandler } from '../utils/userErrorHandler';
+import { ErrorHandler } from '../utils/ErrorHandler';
 
 export class UserController {
   public static async login(req: Request, res: Response) {
@@ -11,7 +11,7 @@ export class UserController {
       const token = await userService.login(email, password);
       res.status(200).json({ token });
     } catch (error) {
-      return UserErrorHandler.handleLoginError(error, res);
+      return ErrorHandler.handle(res, error);
     }
   }
 
@@ -24,16 +24,16 @@ export class UserController {
       const user = await userService.register(name, email, password);
       res.status(201).json(user);
     } catch (error) {
-      return UserErrorHandler.handleRegisterError(error, res);
+      return ErrorHandler.handle(res, error);
     }
   }
 
   public static async getAllUsers(req: Request, res: Response) {
     try {
       const users = await userService.getAllUsers();
-      return res.status(200).json(users);
+      res.status(200).json(users);
     } catch (error) {
-      return UserErrorHandler.handleGetAllUsersError(error, res);
+      return ErrorHandler.handle(res, error);
     }
   }
 }
