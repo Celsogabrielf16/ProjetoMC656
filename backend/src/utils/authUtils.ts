513ec54a6aac2b813e6a1b08966868f2cdde5612
Celso Gabriel Prado <celsogabrielf@gmail.com>
2025-06-08 16:03:32 -0300

    Implementation of extractUserIdFromToken function

--- backend/src/utils/authUtils.ts
+++ backend/src/utils/authUtils.ts
@@ -1,6 +1,15 @@
-// src/utils/authUtils.ts
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (id: number, email: string) => {
     return jwt.sign({ id, email }, process.env.JWT_WORD!, { expiresIn: '30d' });
+};
+
+export const extractUserIdFromToken = (authorizationHeader?: string): number => {
+  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer '))
+    throw new Error('Token ausente ou mal formatado');
+
+  const token = authorizationHeader.split(' ')[1];
+  const payload = jwt.verify(token, process.env.JWT_WORD!) as { id: number };
+  
+  return payload.id;
 };
