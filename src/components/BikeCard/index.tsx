20b551f507428afef52a420f23f568e9903c2a9c
Celso Gabriel Prado <celsogabrielf@gmail.com>
2025-06-21 17:21:20 -0300

    Creation of the BikeCard component

---
+++ src/components/BikeCard/index.tsx
@@ -0,0 +1,32 @@
+import { Bike } from "@/types/bike";
+import styles from './bikeCard.module.scss';
+import Image from 'next/image';
+import BikeImage from '@/assets/bike01.jpg';
+import Star from '@/assets/star.png';
+import Link from "next/link";
+
+export const BikeCard = (bike: Bike) => {
+  return (
+    <Link href={`bike-infos/${bike.id}`} className={styles['bike-card']}>
+      <Image src={BikeImage} className={styles['bike-card__image']} alt="Imagem de uma bicicleta"/>
+      <div className={styles['bike-card__info']}>
+        <div className={styles['bike-card__header']}>
+          <div className={styles['bike-card__model-wrapper']}>
+            <p className={styles['bike-card__model']}>{bike.model}</p>
+            <div className={styles['bike-card__status']}>
+              <div className={styles['bike-card__rating']}>
+                <Image src={Star} alt="Star" />
+                <p>4.8</p>
+              </div>
+              <p>Disponível</p>
+            </div>
+          </div>
+          <div className={styles['bike-card__rate']}>
+            <p>R$ {bike.hourlyRate},00/hora</p>
+          </div>
+        </div>
+        <p className={styles['bike-card__description']}>{bike.description}</p>
+      </div>
+    </Link>
+  )
+}
